9cac5f7d11609f370475695e113caf10f368c781
Dominik Papp <dominik.papp@tcz.cz>
2018-12-07 16:03:11 +0100

    day2 part2

--- day2/main.cpp
+++ day2/main.cpp
@@ -2,13 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <vector>
+#include <string>
 int main()
 {
     FILE *f = fopen("input.txt", "r");
     if(f == NULL)
     {
+        printf("CANNOT OPEN");
         return 1;
     }
+    std::vector<std::string> lines;
+    std::vector<const std::string*> matches;
+    size_t matchpos = 0;
+
     char buffer[32];
 
     int counter['z' - 'a' + 1] = {};
@@ -38,6 +45,7 @@ int main()
         {
             break;
         }
+        lines.push_back(std::string(buffer, read));
         for (char *it = buffer; it != buffer + read; ++it)
         {
             ++counter[int(*it) - 'a'];
@@ -64,6 +72,57 @@ int main()
     while(r > 0);
 
     printf("PART1=%d*%d=%d\n", twos, tres, twos * tres);
+
+
+    for(const auto &line : lines)
+    {
+        for(auto it = lines.cbegin(); it != lines.cend(); ++it)
+        {
+            int diffcount = 0;
+            size_t mp = 0;
+            if((*it).size() == line.size())
+            {
+                for(size_t i = 0; i < line.size(); ++i)
+                {
+                    if(line[i] != (*it)[i])
+                    {
+                        ++ diffcount;
+                        mp = i;
+                    }
+                    if(diffcount > 1)
+                    {
+                        break;
+                    }
+                }
+                if(diffcount == 1)
+                {
+                    matches.push_back(&line);
+                    matchpos = mp;
+                }
+            }
+        }
+    }
+
+    std::cout << "matches: " << matches.size() << " " << matchpos << std::endl;
+    for(auto &m : matches)
+    {
+        std::cout << *m << std::endl;
+    }
+
+    std::cout << "\n\nPART2=" << matches.size() << " : " << matchpos << " - ";
+    auto &mf = matches.front();
+    for(size_t i = 0; i < mf->size(); ++i)
+    {
+        if(i != matchpos)
+        {
+            std::cout << (*mf)[i];
+        }
+    }
+
+    std::cout << std::endl;
+
+
+
 
     return 0;
 }
