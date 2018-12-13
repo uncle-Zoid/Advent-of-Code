0dd806b5e5655ef99b565881cf86a1b98734aa37
Your Name <you@example.com>
2018-12-13 20:21:48 +0100

    day 5 part 2

--- day5/main.cpp
+++ day5/main.cpp
@@ -1,6 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <cstring>
+
+
+int doPolymorf(char *polymorf, const long size)
+{
+    for(char *it = polymorf; it != (polymorf + size - 1); )
+    {
+        char *nextone = it + 1;
+        while(it != (polymorf + size - 1) && *nextone == '.')++nextone;
+
+//        printf("%c %c\n", *it, *nextone);
+        if(tolower(*nextone) == tolower(*it))
+        {
+            if((isupper(*nextone) && islower(*it)) || (isupper(*it) && islower(*nextone)))
+            {
+                *nextone = '.';
+                *it = '.';
+
+                while(it != polymorf && *--it == '.');
+                continue;
+            }
+        }
+        while(it != (polymorf + size - 1) && *++it == '.');
+    }
+
+    int remains = 0;
+    for(char *it = polymorf; it != (polymorf + size); ++it)
+    {
+        if(*it != '.')
+        {
+//            printf("%c", *it);
+            ++remains;
+        }
+
+    }
+    return remains;
+}
+void print(char *polymorf, const long size)
+{
+    for(char *it = polymorf; it != (polymorf + size); ++it)
+    {
+        if(*it != '.')
+        {
+            printf("%c", *it);
+        }
+    }
+    printf("\n\n");
+}
+void removeUnit(char *polymorf, const long size, char unit)
+{
+    unit = tolower(unit);
+    for(char *it = polymorf; it != (polymorf + size); ++it)
+    {
+        if(tolower(*it) == unit)
+        {
+            *it = '.';
+        }
+    }
+}
 
 int main()
 {
@@ -17,48 +76,46 @@ int main()
     rewind(f);
 
 
-    char *polymorf = new char[size+1]();
+    char *polymorf = (char*)calloc(size+1, sizeof(char));
     fread(polymorf, 1, size_t(size), f);
 
 //    printf("%s\n", polymorf);
+    char *polymorf2 = (char*)calloc(size+1, sizeof(char));
+    memcpy(polymorf2, polymorf, size_t(size));
 
-    for(char *it = polymorf; it != (polymorf + size - 1); )
-    {
-        char *nextone = it + 1;
-        while(*nextone == '.')++nextone;
-
-//        printf("%c %c\n", *it, *nextone);
-        if(tolower(*nextone) == tolower(*it))
-        {
-            if((isupper(*nextone) && islower(*it)) || (isupper(*it) && islower(*nextone)))
-            {
-                *nextone = '.';
-                *it = '.';
-
-                while(it != polymorf && *--it == '.');
-                continue;
-            }
-        }
-        while(*++it == '.');
-    }
-
-    int remains = 0;
-    for(char *it = polymorf; it != (polymorf + size); ++it)
-    {
-        if(*it != '.')
-        {
-//            printf("%c", *it);
-            ++remains;
-        }
-
-    }
-
+    int remains = doPolymorf(polymorf2, size);
+//    print(polymorf2, size);
     printf("Part1: Result: %d \n", remains);
-    delete [] polymorf;
+
+    int minlen = 1e6;
+    char minelement;
+
+    for(char i = 'a'; i < 'z'; ++i)
+    {
+        char *polymorf2 = (char*)calloc(size+1, sizeof(char));
+        memcpy(polymorf2, polymorf, size_t(size));
+
+        removeUnit(polymorf2, size, i);
+
+        int res = doPolymorf(polymorf2, size);
+//        print(polymorf2, size);
+        if(res < minlen)
+        {
+            minlen = res;
+            minelement = i;
+        }
+        free(polymorf2);
+    }
+
+
+    printf("PART2: removed unit=%c - final size %d\n", minelement, minlen);
+
+
+    free(polymorf2);
+    free(polymorf);
 
 
 //    printf("PART1: %d most sleep in %d (%d) => %d\n", maxsleptid, mostsleepminute+1, mostsleepminuteCount, maxsleptid * (mostsleepminute+1));
-//    printf("PART2: pocet=%d id=%d\n", ids.size(), *ids.begin());
 
     return 0;
 }
